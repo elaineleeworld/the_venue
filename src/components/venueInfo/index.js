b8b2c7178e232ff84e4ce785f62fe93a4b9d9857
Elaine Lee <elaine.lee@ff0000.com>
2019-05-16 15:42:04 -0700

    using react reveal

---
+++ src/components/venueInfo/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Zoom from "react-reveal/Zoom";
+import icon_calendar from "../../resources/images/icons/calendar.png";
+import icon_location from "../../resources/images/icons/location.png";
+
+const VenueInfo = () => {
+  return (
+    <div className="bck_black">
+      <div className="center_wrapper">
+        <div className="vn_wrapper">
+          <Zoom duration={500}>
+            <div className="vn_item">
+              <div className="vn_outer">
+                <div className="vn_inner">
+                  <div className="vn_icon_square bck_red" />
+                  <div className="vn_icon" style={{ background: `url(${icon_calendar})` }} />
+                  <div className="vn_title">Event Date & Time</div>
+                  <div className="vn_desc">
+                    7 August 2019 <br /> @10:00PM
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Zoom>
+          <Zoom duration={500} delay={500}>
+            <div className="vn_item">
+              <div className="vn_outer">
+                <div className="vn_inner">
+                  <div className="vn_icon_square bck_red" />
+                  <div className="vn_icon" style={{ background: `url(${icon_calendar})` }} />
+                  <div className="vn_title">Event Location</div>
+                  <div className="vn_desc">2301 N Highland Ave, LA, CA 90068</div>
+                </div>
+              </div>
+            </div>
+          </Zoom>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VenueInfo;
