78e1ffda449ebe53802544fbb3e5dd0b2a7c45eb
Elaine Lee <elaine.lee@ff0000.com>
2019-05-16 22:33:06 -0700

    highlights discount description components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./resources/styles.css";
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
 import VenueInfo from "./components/venueInfo";
+import Highlight from "./components/Highlights";
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "1500px", background: "cornflowerblue" }}>
+      <div className="App" style={{ height: "1500px" }}>
         <Header />
         <Featured />
         <VenueInfo />
+        <Highlight />
       </div>
     );
   }
